eb0f398a1483896a50b1347468f1d15b1048bb25
Michael B <netopya@netopyaplanet.com>
2016-12-27 16:40:43 -0500

    Updated links page
    -Also moved css files to the css folder

--- links.php
+++ links.php
@@ -7,41 +7,35 @@ include("opening_php.php");
 ?><!DOCTYPE html>
 
 <html>
-<head>
-    <title><?php echo $links_title[$refined_laguage]; ?></title>
-    <LINK REL=StyleSheet HREF="stylesheet1.css" TYPE="text/css">
-    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-   <script type="text/javascript" src="common_scripts.js"></script>
-   <meta name="keywords" content="Ukrainian,Catholic,Holy,Ghost,Church,Montreal,église,catholique,ukrainienne,Saint-Esprit,Montréal,Українська,Католицька,Церква,Святого,Духа,Монреалі">
-</head>
-
-<body>
-<div id="container_main">
-    <div id="heading_container">
-        <div id="heading_content">
-            <?php
-            include("header1.php");
-            ?>
+    <head>
+        <title><?php echo $links_title[$refined_laguage]; ?></title>
+        <?php include("php/head.php"); ?>
+    </head>
+    <body>
+        <?php include("php/navbar1.php"); ?>
+        
+        <div id="container_main">
+            <div id="smalltopnavback"></div>
+            <div id="webcontent_background">
+                <div class="container marketing">
+                    <div class="row">
+                        <div class="col-lg-10 col-lg-offset-1">
+                            <div class="page-header">
+                                <h1><?php echo $link_button[$refined_laguage];  ?></h1>
+                            </div>
+                            <p>
+                                <?php echo $link1[$refined_laguage]; ?><br/> <a href="http://www.ugcc.org.ua">http://www.ugcc.org.ua</a></br></br>
+                                <?php echo $link2[$refined_laguage]; ?><br/> <a href="http://www.ucet.ca/">http://www.ucet.ca/</a></br></br>
+                                <?php echo $link3[$refined_laguage]; ?><br/> <a href="http://www.ukrainiantime.com">http://www.ukrainiantime.com</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div id="footer">
+            
+            </div>
         </div>
-        
-        
-    </div>
-    <div id="webcontent_background">
-    <div id="webcontent_container">
-        <div class="blog_post">
-            <h1><?php echo $link_button[$refined_laguage]; ?></h1>
-            <?php echo $link1[$refined_laguage]; ?><br/> <a href="http://www.ugcc.org.ua">http://www.ugcc.org.ua</a></br></br>
-            <?php echo $link2[$refined_laguage]; ?><br/> <a href="http://www.ucet.ca/">http://www.ucet.ca/</a></br></br>
-            <?php echo $link3[$refined_laguage]; ?><br/> <a href="http://www.ukrainiantime.com">http://www.ukrainiantime.com</a>
-        </div>
-        
-    </div>
-    </div>
-    <div id="footer">
-        
-    </div>
-</div>
-
-
-</body>
+    </body>
 </html>
