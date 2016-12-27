45f7412e03d26f1134e84c6e0b6aa036827c5552
Michael B <netopya@netopyaplanet.com>
2016-12-27 15:52:04 -0500

    Finished about page
    -Also moved out header and nav code to individual files

--- about.php
+++ about.php
@@ -1,45 +1,34 @@
 <?php
 
-include("opening_php.php");
+    include("opening_php.php");
 
     $pagebutton_id = 2;
 
 ?><!DOCTYPE html>
-
 <html>
-<head>
-    <title><?php echo $about_title[$refined_laguage]; ?></title>
-    <LINK REL=StyleSheet HREF="stylesheet1.css" TYPE="text/css">
-    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-   <script type="text/javascript" src="common_scripts.js"></script>
-   <meta name="keywords" content="Ukrainian,Catholic,Holy,Ghost,Church,Montreal,église,catholique,ukrainienne,Saint-Esprit,Montréal,Українська,Католицька,Церква,Святого,Духа,Монреалі">
-</head>
-
+    <head>
+        <title><?php echo $about_title[$refined_laguage]; ?></title>
+        <?php include("php/head.php"); ?>
+    </head>
 <body>
-<div id="container_main">
-    <div id="heading_container">
-        <div id="heading_content">
-            <?php
-            include("header1.php");
-            ?>
+    <?php include("php/navbar1.php"); ?>
+    
+    <div id="container_main">
+        <div id="smalltopnavback"></div>
+        <div id="webcontent_background">
+            <div class="container marketing">
+                <div class="row">
+                    <div class="col-lg-10 col-lg-offset-1">
+                        <h1><?php echo $about_text_title[$refined_laguage];  ?></h1>
+                        <p><?php echo $about_text[$refined_laguage]; ?></p>
+                    </div>
+                </div>
+            </div>
         </div>
-        
-        
-    </div>
-    <div id="webcontent_background">
-    <div id="webcontent_container">
-        <div class="blog_post">
-            <h1><?php echo $about_text_title[$refined_laguage];  ?></h1>
-            <p><?php echo $about_text[$refined_laguage]; ?></p>
+        <div id="footer">
             
         </div>
-        
     </div>
-    </div>
-    <div id="footer">
-        
-    </div>
-</div>
 
 
 </body>
--- index.php
+++ index.php
@@ -8,81 +8,19 @@
 <html>
     <head>
         <title><?php echo $home_title[$refined_laguage]; ?></title>
-
-        <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-        <script type="text/javascript" src="common_scripts.js"></script>
-
-        <meta name="keywords" content="Ukrainian,Catholic,Holy,Ghost,Church,Montreal,église,catholique,ukrainienne,Saint-Esprit,Montréal,Українська,Католицька,Церква,Святого,Духа,Монреалі">
-
-        <link rel="stylesheet" href="css/flags.css">
         
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-        <link rel="stylesheet" href="bootstrap.override.css">
-        
-        <LINK REL=StyleSheet HREF="stylesheet1.css" TYPE="text/css">
+        <?php include("php/head.php"); ?>
     </head>
 
 <body>
-    
+    <?php include("php/navbar1.php"); ?>
     <div id="container_main">
-        <div class="navbar-wrapper">
-            <div class="container">
-                <nav class="navbar navbar-default">
-                  <div class="container-fluid">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header">
-                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                      </button>
-                      <a class="navbar-brand" href="index.php">UCHGC</a>
-                    </div>
 
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                      <ul class="nav navbar-nav">
-                        <li class="active"><a href="index.php"><?php echo $home_button[$refined_laguage]; ?></a></li>
-                        <li ><a href="about.php"><?php echo $about_button[$refined_laguage]; ?></a></li>
-                        <li ><a href="photos.php"><?php echo $photo_button[$refined_laguage]; ?></a></li>
-                        <li ><a href="location.php"><?php echo $location_button[$refined_laguage]; ?></a></li>
-                        <li ><a href="contact.php"><?php echo $contact_button[$refined_laguage]; ?></a></li>
-                        <li ><a href="links.php"><?php echo $link_button[$refined_laguage]; ?></a></li>
-                      </ul>
-                      <ul class="nav navbar-nav navbar-right">
-                        <li class="dropdown">
-                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $language_label[$refined_laguage];?> <span class="flag flag-gb"></span> <span class="flag flag-fr"></span> <span class="flag flag-ua"></span> <span class="caret"></span></a>
-                          <ul class="dropdown-menu">
-                            <li onclick="language_changed(1)"><a href="#"><span class="flag flag-gb"></span> English</a></li>
-                            <li onclick="language_changed(2)"><a href="#"><span class="flag flag-fr"></span> Français</a></li>
-                            <li onclick="language_changed(3)"><a href="#"><span class="flag flag-ua"></span> Український</a></li>
-                          </ul>
-                        </li>
-                      </ul>
-                    </div><!-- /.navbar-collapse -->
-                  </div><!-- /.container-fluid -->
-                </nav>
-            </div>
-        </div>
         <!-- carousel begin -->
         <div id="main-carousel" class="carousel slide" data-ride="carousel">
           <!-- Indicators -->
           <ol class="carousel-indicators">
             <li data-target="#main-carousel" data-slide-to="0" class="active"></li>
-            <li data-target="#main-carousel" data-slide-to="1"></li>
           </ol>
 
           <!-- Wrapper for slides -->
@@ -92,13 +30,6 @@
               <div class="carousel-caption">
                 <!--<h1></h1>
                 <p></br></p>-->
-              </div>
-            </div>
-            <div class="item">
-              <img src="st_esprit_int1 - Copy.jpg" alt="...">
-              <div class="carousel-caption">
-                <h1>NETOPYAPLANET.COM</h1>
-                <p>Welcome to netopyaplanet.com!</br>Programming, projects, and more ...</p>
               </div>
             </div>
           </div>
