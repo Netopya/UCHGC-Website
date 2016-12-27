3eb70d967174f5c85a369fab0390261c22d56dc5
Michael B <netopya@netopyaplanet.com>
2016-12-27 13:53:29 -0500

    Initial bootstrap integration
    -added basic nav bar and header

--- index.php
+++ index.php
@@ -15,26 +15,96 @@
     <title><?php echo $home_title[$refined_laguage]; ?></title>
     <LINK REL=StyleSheet HREF="stylesheet1.css" TYPE="text/css">
     <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-   <!--<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">-->
-   <script type="text/javascript" src="common_scripts.js"></script>
-   <script type="text/javascript">
     
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     
-   </script>
-   <meta name="keywords" content="Ukrainian,Catholic,Holy,Ghost,Church,Montreal,église,catholique,ukrainienne,Saint-Esprit,Montréal,Українська,Католицька,Церква,Святого,Духа,Монреалі">
+    <script type="text/javascript" src="common_scripts.js"></script>
+    
+    <meta name="keywords" content="Ukrainian,Catholic,Holy,Ghost,Church,Montreal,église,catholique,ukrainienne,Saint-Esprit,Montréal,Українська,Католицька,Церква,Святого,Духа,Монреалі">
+    
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" href="bootstrap.override.css">
 </head>
 
 <body>
 <div id="container_main">
-    <div id="heading_container">
-        <div id="heading_content">
-            <?php
-            include("header1.php");
-            ?>
+    <div class="navbar-wrapper">
+        <div class="container">
+            <nav class="navbar navbar-default">
+              <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="index.php">UCHGC</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php"><?php echo $home_button[$refined_laguage]; ?></a></li>
+                    <li ><a href="about.php"><?php echo $about_button[$refined_laguage]; ?></a></li>
+                    <li ><a href="photos.php"><?php echo $photo_button[$refined_laguage]; ?></a></li>
+                    <li ><a href="location.php"><?php echo $location_button[$refined_laguage]; ?></a></li>
+                    <li ><a href="contact.php"><?php echo $contact_button[$refined_laguage]; ?></a></li>
+                    <li ><a href="links.php"><?php echo $link_button[$refined_laguage]; ?></a></li>
+                  </ul>
+                </div><!-- /.navbar-collapse -->
+              </div><!-- /.container-fluid -->
+            </nav>
         </div>
-        
-        
-    </div>
+    </div>	
+    <!-- carousel begin -->
+    <div id="main-carousel" class="carousel slide" data-ride="carousel">
+	  <!-- Indicators -->
+	  <ol class="carousel-indicators">
+		<li data-target="#main-carousel" data-slide-to="0" class="active"></li>
+		<li data-target="#main-carousel" data-slide-to="1"></li>
+	  </ol>
+
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner" role="listbox">
+		<div class="item active">
+		  <img src="st_esprit_int1 - Copy.jpg" alt="...">
+		  <div class="carousel-caption">
+			<!--<h1></h1>
+			<p></br></p>-->
+		  </div>
+		</div>
+        <div class="item">
+		  <img src="st_esprit_int1 - Copy.jpg" alt="...">
+		  <div class="carousel-caption">
+			<h1>NETOPYAPLANET.COM</h1>
+			<p>Welcome to netopyaplanet.com!</br>Programming, projects, and more ...</p>
+		  </div>
+		</div>
+	  </div>
+
+	  <!-- Controls -->
+	  <a class="left carousel-control" href="#main-carousel" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	  </a>
+	  <a class="right carousel-control" href="#main-carousel" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	  </a>
+	</div>
+	
+	<!-- END CAROUSEL -->
+    
     <div id="webcontent_background">
     <div id="webcontent_container">
         
@@ -179,12 +249,13 @@
         </div>
         -->
     </div>
-</div>
-    <div id="footer">
-        
     </div>
+        <div id="footer">
+            
+        </div>
+    </div>
+
 </div>
-
-
+</div>
 </body>
 </html>
