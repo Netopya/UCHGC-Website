dcb98c55bb5a947591db2ec89d9cecf3c60b382f
Michael B <netopya@netopyaplanet.com>
2016-12-27 16:28:32 -0500

    Updated contact page

--- contact.php
+++ contact.php
@@ -7,42 +7,39 @@ include("opening_php.php");
 ?><!DOCTYPE html>
 
 <html>
-<head>
-    <title><?php echo $contact_title[$refined_laguage]; ?></title>
-    <LINK REL=StyleSheet HREF="stylesheet1.css" TYPE="text/css">
-    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-   <script type="text/javascript" src="common_scripts.js"></script>
-   <meta name="keywords" content="Ukrainian,Catholic,Holy,Ghost,Church,Montreal,église,catholique,ukrainienne,Saint-Esprit,Montréal,Українська,Католицька,Церква,Святого,Духа,Монреалі">
-</head>
-
-<body>
-<div id="container_main">
-    <div id="heading_container">
-        <div id="heading_content">
-            <?php
-            include("header1.php");
-            ?>
+    <head>
+        <title><?php echo $contact_title[$refined_laguage]; ?></title>
+        <?php include("php/head.php"); ?>
+    </head>
+    <body>
+        <?php include("php/navbar1.php"); ?>
+        
+        <div id="container_main">
+            <div id="smalltopnavback"></div>
+            <div id="webcontent_background">
+                <div class="container marketing">
+                    <div class="row">
+                        <div class="col-lg-10 col-lg-offset-1">
+                            <div class="page-header">
+                                <h1><?php echo $contact_button[$refined_laguage];  ?></h1>
+                            </div>
+                            <div class="contact_person">
+                                <img class="img-thumbnail" src="contact/thb_V_Vitt.jpg"/>
+                                <address>
+                                    <h3><?php echo $priestname[$refined_laguage]; ?></h3>
+                                    <?php echo $priestresidence[$refined_laguage]; ?>: 7345 Churchill Verdun Qc <br>
+                                    <?php echo $telephone[$refined_laguage]; ?>: 514-769-3804<br>
+                                    <?php echo $email[$refined_laguage]; ?>: <a href="mailto:v.vitt@hotmail.com">v.vitt@hotmail.com</a>
+                                </address>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div id="footer">
+            
+            </div>
         </div>
-        
-        
-    </div>
-    <div id="webcontent_background">
-    <div id="webcontent_container">
-        <h1><?php echo $contact_button[$refined_laguage]; ?></h1>
-        <div class="contact_person">
-            <img id="#priestimage" src="contact/thb_V_Vitt.jpg"/>
-            <h3><?php echo $priestname[$refined_laguage]; ?></h3>
-            <p><?php echo $priestresidence[$refined_laguage]; ?>: 7345 Churchill Verdun Qc </br><?php echo $telephone[$refined_laguage]; ?>: 514-769-3804</br>
-            <?php echo $email[$refined_laguage]; ?>: <a href="mailto:v.vitt@hotmail.com">v.vitt@hotmail.com</p>
-        </div>
-        
-    </div>
-    </div>
-    <div id="footer">
-        
-    </div>
-</div>
-
-
-</body>
+    </body>
 </html>
