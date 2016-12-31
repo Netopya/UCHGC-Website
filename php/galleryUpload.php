ce6aea46ccfcee42296500934e504510d366ff00
Michael B <netopya@netopyaplanet.com>
2016-12-31 17:37:03 -0500

    Reminder

--- php/galleryUpload.php
+++ php/galleryUpload.php
@@ -68,6 +68,8 @@
         //Setup our new file path
         $newFilePath = "uploadFiles/" . $_FILES['userfiles']['name'][$i];
 
+        // Handle error case where directory doesn't exist and this would print an error!
+        
         //Upload the file into the temp dir
         if(move_uploaded_file($tmpFilePath, $newFilePath)) {
 
