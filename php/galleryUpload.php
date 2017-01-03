d43edec2a9ab1ff47e97e8b3c3147f995dcb24c6
Michael B <netopya@netopyaplanet.com>
2017-01-03 17:19:38 -0500

    Progress on file upload
    -Construction of file name and directory allong with the association in
    the db

--- php/galleryUpload.php
+++ php/galleryUpload.php
@@ -49,6 +49,10 @@
     {
         dieError("Could not find the required gallery");
     }
+    else
+    {
+        $stmt->fetch();
+    }    
 
     // Count # of uploaded files in array
     $total = count($_FILES['userfiles']['name']);
@@ -57,7 +61,14 @@
     {
         dieError("No files sent");
     }
-    
+
+    $dir = "../gallery_images/" . $cleanId . "/";
+
+    if( is_dir($dir) === false )
+    {
+        mkdir($dir);
+    }
+
     // Loop through each file
     for($i=0; $i<$total; $i++) {
       //Get the temp file path
@@ -66,20 +77,31 @@
       //Make sure we have a filepath
       if ($tmpFilePath != ""){
         //Setup our new file path
-        $newFilePath = "uploadFiles/" . $_FILES['userfiles']['name'][$i];
-
+        $filePath = $_FILES['userfiles']['name'][$i];
+        $ext = strtolower (pathinfo($filePath, PATHINFO_EXTENSION));
+        $fileName = pathinfo($_FILES['userfiles']['name'][$i], PATHINFO_FILENAME);
+        $fileName = preg_replace('/\s+/', '', $fileName);
         // Handle error case where directory doesn't exist and this would print an error!
         
+        $stmt = $conn->prepare("INSERT INTO GalleryImages (gallery_id, imagename, extension, userid) VALUES (?,?,?,?)");
+        $stmt->bind_param("ssss", $cleanId, $fileName, $ext, $_SESSION["userId"]);
+        $success = $stmt->execute();
+        
+        $imageId = $conn->insert_id;
+        
+        $fullName = $fileName . "_" . $imageId . "." . $ext;
+
         //Upload the file into the temp dir
-        if(move_uploaded_file($tmpFilePath, $newFilePath)) {
+        if(move_uploaded_file($tmpFilePath, $dir . $fullName)) {
 
           //Handle other code here
-
+            
         }
         else
         {
-            // Create some way to add the file to failed uploads
+            //TODO: Create some sort of soft error handling
+            dieError("Could not upload image: " . $fileName);
         }
       }
-}
+    }
 ?>
